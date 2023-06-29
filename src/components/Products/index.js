9c3c5b9cd069dcca058780554ff3f36206592617
Juan Esteban Gomez Cardona <104547356+juanesgomezcod@users.noreply.github.com>
2023-06-29 09:36:36 -0500

    productos

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,9 +1,33 @@
 import React from "react"
+import Logo from "../../images/Logo.jpg";
 
 export const ProductsList = () => {
     return (
-        <div>
-            <h1>Productos</h1>
+        <>
+        <h1 class="title">PRODUCTOS</h1>
+        <div className="products">
+            <div class="product">
+            <a href="#">
+            <div class="product__img">
+                <img src="{Logo}" alt="logo"/>
+            </div>
+            </a>
+            <div class="product__footer">
+                <h1>Title</h1>
+                <p>Categoria</p>
+                <p class="price">$60.000</p>
+            </div>
+            <div class="button">
+                <button className="btn">
+                    Añadir al carrito
+                </button>
+                <div>
+                    <a href="#" className="btn">Vista</a>
+                </div>
+
+            </div>
+            </div>
         </div>
+        </>
     )
 }
