5218a9a04d59c4b4197c7bcea754b834b2ffa3bf
Juan Esteban Gomez Cardona <104547356+juanesgomezcod@users.noreply.github.com>
2023-06-30 16:59:18 -0500

    mas de estilos productos

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,33 +1,116 @@
-import React from "react"
-import Logo from "../../images/Logo.jpg";
+import React from "react";
+import IMG from "../../images/Radiador.jpg";
 
 export const ProductsList = () => {
-    return (
-        <>
-        <h1 class="title">PRODUCTOS</h1>
-        <div className="products">
-            <div class="product">
-            <a href="#">
+  return (
+    <>
+      <h1 class="title">CATALOGO</h1>
+      <div className="products">
+        <div class="product">
+          <a href="#">
             <div class="product__img">
-                <img src="{Logo}" alt="logo"/>
+              <img src="{IMG}" alt="Radiador" />
             </div>
+          </a>
+          <div class="product__footer">
+            <h1>Title</h1>
+            <p>Categoria</p>
+            <p class="price">$60.000</p>
+          </div>
+          <div class="button">
+            <button className="btn">Añadir al carrito</button>
+            <div>
+              <a href="#" className="btn">
+                Vista
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="product">
+            <a href="#">
+              <div class="product__img">
+                <img src="{IMG}" alt="Radiador" />
+              </div>
             </a>
             <div class="product__footer">
-                <h1>Title</h1>
-                <p>Categoria</p>
-                <p class="price">$60.000</p>
+              <h1>Title</h1>
+              <p>Categoria</p>
+              <p class="price">$60.000</p>
             </div>
             <div class="button">
-                <button className="btn">
-                    Añadir al carrito
-                </button>
-                <div>
-                    <a href="#" className="btn">Vista</a>
-                </div>
+              <button className="btn">Añadir al carrito</button>
+              <div>
+                <a href="#" className="btn">
+                  Vista
+                </a>
+              </div>
+            </div>
+          </div>
 
+          <div class="product">
+            <a href="#">
+              <div class="product__img">
+                <img src="{IMG}" alt="Radiador" />
+              </div>
+            </a>
+            <div class="product__footer">
+              <h1>Title</h1>
+              <p>Categoria</p>
+              <p class="price">$60.000</p>
             </div>
+            <div class="button">
+              <button className="btn">Añadir al carrito</button>
+              <div>
+                <a href="#" className="btn">
+                  Vista
+                </a>
+              </div>
             </div>
-        </div>
-        </>
-    )
-}
+          </div>
+
+          <div class="product">
+            <a href="#">
+              <div class="product__img">
+                <img src="{IMG}" alt="Radiador" />
+              </div>
+            </a>
+            <div class="product__footer">
+              <h1>Title</h1>
+              <p>Categoria</p>
+              <p class="price">$60.000</p>
+            </div>
+            <div class="button">
+              <button className="btn">Añadir al carrito</button>
+              <div>
+                <a href="#" className="btn">
+                  Vista
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="product">
+            <a href="#">
+              <div class="product__img">
+                <img src="{IMG}" alt="Radiador" />
+              </div>
+            </a>
+            <div class="product__footer">
+              <h1>Title</h1>
+              <p>Categoria</p>
+              <p class="price">$60.000</p>
+            </div>
+            <div class="button">
+              <button className="btn">Añadir al carrito</button>
+              <div>
+                <a href="#" className="btn">
+                  Vista
+                </a>
+              </div>
+            </div>
+          </div>
+      </div>
+    </>
+  );
+};
